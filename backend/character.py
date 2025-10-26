5e61e5deae07b3ceae93cf41ecaa04cfc8d794c3
Ritam Saha <ritamsaha2005@gmail.com>
2025-10-25 23:38:40 -0700

    omg working

--- backend/character.py
+++ backend/character.py
@@ -1,17 +1,12 @@
 class Character:
-    def __init__(self, name, age, gender):
-        self.name = name # uuid
-        self.age = age
-        self.gender = gender
-        self.context = {
-            1: "i am a cool person"
-        }
+    def __init__(self, name, context):
+        self.name: str = name
+        self.context: str = context
 
-# 2 levels of context
-    # some levels 
+
 class Relationship:
-    def __init__(self, name, relationship_type, context):
-        self.name = name
-        self.relationship_type = ""
-        self.intimacy = 1
-        self.context = "" # system + level2 + boy1 + boy2 + level3
+    def __init__(self, name1, name2, context, reveals):
+        self.name1 = name1
+        self.name2 = name2
+        self.context = context
+        self.reveals: List[str] = reveals
