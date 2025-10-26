69d039444335414cc6b502840ff48ea1b8c821a9
Sabrina Yen-Ko <59457991+syenko@users.noreply.github.com>
2025-10-26 06:10:49 -0700

    Start page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,30 @@
+"use client";
 import Background from "@/components/Background";
-import Link from "next/link";
+import PixelButton from "@/components/PixelButton";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const router = useRouter();
     return (
         <div>
-            <Background src={"/backgrounds/outside.png"} opacity={100} />
-            <div className="text-4xl text-white font-bold">
-                <Link href="/select">Start</Link>
+            <Background src={"/backgrounds/darkisland.png"} opacity={100} />
+            <div className="flex flex-col items-center justify-center h-screen text-white">
+                <div
+                    className="text-5xl text-white leading-10"
+                    style={{ fontFamily: "PixelScript" }}
+                >
+                    Love Island
+                </div>
+                <div className="text-4xl py-10">To Die For...</div>
+                <PixelButton
+                    onClick={() => router.push("/select")}
+                    disabled={false}
+                    height={60}
+                    width={200}
+                    color="green"
+                >
+                    Click to Start
+                </PixelButton>
             </div>
         </div>
     );
