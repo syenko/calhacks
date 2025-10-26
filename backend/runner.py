81841389d7edb13641d25053f34b909688446e6f
Ritam Saha <ritamsaha2005@gmail.com>
2025-10-26 02:35:24 -0700

    integrate new relationships to multichar chat context

--- backend/runner.py
+++ backend/runner.py
@@ -19,51 +19,58 @@ def main():
         for char in characters_raw
     ]
 
-    relationships = [
-        Relationship(rel["name1"], rel["name2"], rel["context"], rel["reveals"])
+    relationships = {
+        Relationship(rel["name1"], rel["name2"], rel["background"], rel["reveals"])
         for rel in relationships_raw
-    ]
+    }
+
+    relationships_map = {
+        tuple((rel.name1, rel.name2)): rel.reveals
+        for rel in relationships
+    }
+
+    message_history = {
+        "Daisy": [],
+        "Sienna": [],
+        "Grace": [],
+        "Drew": [],
+    }
     
     game = Game(user, characters, relationships)
 
     # daisy + sienna
-    start_multicharacter_chat(characters[0], characters[1], user)
+    # TODO: optimize relationships passthrough
+    start_multicharacter_chat(characters[0], characters[1], user, relationships_map)
 
     # daisy + drew
-    start_multicharacter_chat(characters[0], characters[3], user)
+    start_multicharacter_chat(characters[0], characters[3], user, relationships_map)
 
     # for character in characters:
     #     start_chat(character, user)
 
 
-def start_chat(character: Character, user: Character):
-    user_input = input(f"What do you want to say to {character.name}?: ") # query front end
-    system = f"You are {character.name} and you are talking to {user.name}."
-    messages = [
-        {"role": "system", "content": system},
-        {"role": "user", "content": character.context},
-        {"role": "user", "content": f"{user.name}: {user_input}"}
-    ]
-    while user_input != 'q':
-        response = get_chat_completion(messages)
-        print(response) # query front end
-        user_input = input(f"What do you want to say to {character.name}? (enter 'q' to quit): ") #query front end
-        messages.append({"role": "assistant", "content": response})
-        messages.append({"role": "user", "content": f"{user.name}: {user_input}"})
-            
-
-def start_multicharacter_chat(character_one: Character, character_two: Character, user: Character):
+def start_multicharacter_chat(character_one: Character, character_two: Character, user: Character, relmap: dict[tuple[Character, Character], str]):
+    # TODO change to query frontend
     user_input = input(f"What do you want to say to {character_one.name} and {character_two.name}?: ") # query front end
     system = f"You are {character_one.name} and {character_two.name} and you are talking to {user.name}."
+
     messages_one = [
         {"role": "system", "content": system},
         {"role": "user", "content": character_one.context},
     ]
 
+    if (character_one, character_two) in relmap:
+        messages_one.append({"role": "user", "content": relmap[(character_one, character_two)]})
+
     messages_two = [
         {"role": "system", "content": system},
         {"role": "user", "content": character_two.context},
     ]
+
+    if (character_two, character_one) in relmap:
+        messages_two.append({"role": "user", "content": relmap[(character_two, character_one)]})
+
+
     while user_input != 'q':
         # 1. randomly select who gets to talk first
         speaker_order = [(character_one, messages_one, character_two, messages_two),
@@ -90,6 +97,28 @@ def start_multicharacter_chat(character_one: Character, character_two: Character
         user_input = input(
             f"What do you want to say to {character_one.name} and {character_two.name}? (enter 'q' to quit): "
         ) # query front end
+
+    # TODO update message history w/ summary (?) after each round
+    # message_history[character_one.name].append(messages_one)
+    # message_history[character_two.name].append(messages_two)
+
+
+
+def start_chat(character: Character, user: Character):
+    user_input = input(f"What do you want to say to {character.name}?: ") # query front end
+    system = f"You are {character.name} and you are talking to {user.name}."
+    messages = [
+        {"role": "system", "content": system},
+        {"role": "user", "content": character.context},
+        {"role": "user", "content": f"{user.name}: {user_input}"}
+    ]
+    while user_input != 'q':
+        response = get_chat_completion(messages)
+        print(response) # query front end
+        user_input = input(f"What do you want to say to {character.name}? (enter 'q' to quit): ") #query front end
+        messages.append({"role": "assistant", "content": response})
+        messages.append({"role": "user", "content": f"{user.name}: {user_input}"})
+            
 
 if __name__ == "__main__":
     main()
