b0448b19b5f9a75e312b5677a65531c494a39c41
yuuuyan <kate.wangtogi@gmail.com>
2025-10-25 22:38:20 -0700

    prelim backend updates: type formats and context eng

--- backend/character.py
+++ backend/character.py
@@ -1,18 +1,17 @@
 class Character:
-    def __init__(self, name, age, gender)
-        self.name = name
+    def __init__(self, name, age, gender):
+        self.name = name # uuid
         self.age = age
         self.gender = gender
-        self.relationships = []
+        self.context = {
+            1: "i am a cool person"
+        }
 
-
-    def add_relationship(self, relationship):
-        pass
-
-
+# 2 levels of context
+    # some levels 
 class Relationship:
-    def __init__(self, name, relationship_type):
+    def __init__(self, name, relationship_type, context):
         self.name = name
         self.relationship_type = ""
         self.intimacy = 1
-        self.context = ""
+        self.context = "" # system + level2 + boy1 + boy2 + level3
