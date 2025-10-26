12828ec2d95b2d75f28effa8a99fee42adee70f7
Ritam Saha <ritamsaha2005@gmail.com>
2025-10-25 20:53:03 -0700

    gang shit

---
+++ backend/character.py
@@ -0,0 +1,18 @@
+class Character:
+    def __init__(self, name, age, gender)
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.relationships = []
+
+
+    def add_relationship(self, relationship):
+        pass
+
+
+class Relationship:
+    def __init__(self, name, relationship_type):
+        self.name = name
+        self.relationship_type = ""
+        self.intimacy = 1
+        self.context = ""
